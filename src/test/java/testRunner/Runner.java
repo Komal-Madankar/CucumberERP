d99d3145bced6714335b1877b4e92402724b9c5e
hp5cd <hp5cd@LAPTOP-6NKJ6NHI>
2024-04-24 13:37:51 -0400

    First commit for cucumber project

---
+++ src/test/java/testRunner/Runner.java
@@ -0,0 +1,14 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"FeatureFiles"},monochrome = true, glue = {"StepDefination"}, strict = true, tags = {"@supplier"},plugin = {"pretty", "html:target/cucumber-reports"})
+
+public class Runner {
+
+}
+
